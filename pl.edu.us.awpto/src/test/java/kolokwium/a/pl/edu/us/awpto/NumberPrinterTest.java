5fb5492cbbd203a0021b35efef637be665f35a2d
Your Name <you@example.com>
2017-07-01 10:22:32 +0200

    Add tests

--- pl.edu.us.awpto/src/test/java/kolokwium/a/pl/edu/us/awpto/NumberPrinterTest.java
+++ pl.edu.us.awpto/src/test/java/kolokwium/a/pl/edu/us/awpto/NumberPrinterTest.java
@@ -2,8 +2,24 @@ package kolokwium.a.pl.edu.us.awpto;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class NumberPrinterTest
 {
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	  
+	@Test
+	 public void printNumbers() {
+		
+	    exception.expect(RuntimeException.class);
+	    exception.expectMessage("limit must be >= 1");
+	    
+	    NumberPrinter tester = new NumberPrinter(null, null);
+	    tester.printNumbers(0);
+	 	
+	 }
 }
